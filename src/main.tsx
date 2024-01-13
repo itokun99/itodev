9455f1ec90a95bd5e27ffbb607538c9b08f4f635
Indrawan Lisanto <indrawanlisantopersonal@gmail.com>
2024-01-13 23:19:10 +0700

    setup dir & setup absolute path

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { MainApp } from '@apps';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <MainApp />
   </React.StrictMode>,
 )
---
+++ src/presentation/apps/main/index.tsx
@@ -0,0 +1,5 @@
+export function MainApp() {
+  return (
+    <div>Hellow world</div>   
+  )
+} 
